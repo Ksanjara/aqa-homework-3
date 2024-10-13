bc322104bd8eb75549333f21ad1698f0d4b8e45e
Kseniia Kuznetsova <ksandrasky@ya.ru>
2024-10-13 17:44:50 +0300

    mistakes corrected

--- src/test/java/FillFormTests.java
+++ src/test/java/FillFormTests.java
@@ -3,9 +3,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 
@@ -16,12 +14,16 @@ public class FillFormTests {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = false;
     }
 
     @Test
     void fillForm(){
         open("https://demoqa.com/automation-practice-form");
+
+        //remove banners
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
 
         //fill Name and Surname
         $("#firstName").setValue("Victoriia");
@@ -30,39 +32,52 @@ public class FillFormTests {
         //fill email
         $("#userEmail").setValue("super@email.ru");
 
-        //select sex
-        $(byText("Female")).click();
+        //select gender
+        $("#genterWrapper").findElement(byText("Female")).click();
 
         //enter phone number
         $("#userNumber").setValue("1234567890");
 
         //select date of birth
-        $("#dateOfBirthInput").setValue("17 Jan 2024").pressEnter();
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOption("1995");
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__month").$(byText("17")).click();
 
         //select subjects
         $("#subjectsInput").val("Maths").pressEnter();
         $("#subjectsInput").val("Chemistry").pressEnter();
 
         //select interests
-        $(byText("Sports")).click();
+        $("#hobbiesWrapper").findElement(byText("Sports")).click();
 
         //upload picture
-        $("#uploadPicture").uploadFile(new File("C:\\Users\\Cherry\\Desktop\\Google_Test_passed.png"));
+        $("#uploadPicture").uploadFromClasspath("Google_Test_passed.png");
 
         //enter address
         $("#currentAddress").setValue("some street \n and some house. Maybe 2?");
 
         //State and City fields
         $("#state").scrollIntoView(true).click();
-        $(byText("Uttar Pradesh")).click();
+        $("#state").$(byText("Uttar Pradesh")).click();
         $("#city").click();
-        $(byText("Lucknow")).click();
+        $("#city").$(byText("Lucknow")).click();
 
         //Submit form
         $("#submit").click();
 
         //checking result
         $("#example-modal-sizes-title-lg").shouldHave(Condition.text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(Condition.text("Victoriia Mansurova"));
+        $(".table-responsive").shouldHave(Condition.text("super@email.ru"));
+        $(".table-responsive").shouldHave(Condition.text("Female"));
+        $(".table-responsive").shouldHave(Condition.text("1234567890"));
+        $(".table-responsive").shouldHave(Condition.text("17 January,1995"));
+        $(".table-responsive").shouldHave(Condition.text("Maths, Chemistry"));
+        $(".table-responsive").shouldHave(Condition.text("Sports"));
+        $(".table-responsive").shouldHave(Condition.text("Google_Test_passed.png"));
+        $(".table-responsive").shouldHave(Condition.text("some street and some house. Maybe 2?"));
+        $(".table-responsive").shouldHave(Condition.text("Uttar Pradesh Lucknow"));
 
     }
 }
