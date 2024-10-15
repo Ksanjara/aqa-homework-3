3c93f1f948d596b8fd97427847cd6ca1c5913233
Kseniia Kuznetsova <ksandrasky@ya.ru>
2024-10-15 15:01:56 +0300

    githubWikiTest added

---
+++ src/test/java/GithubWikiTest.java
@@ -0,0 +1,41 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+public class GithubWikiTest {
+
+    @BeforeAll
+    static void preconditionsForAllTests() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = false;
+    }
+
+    @Test
+    void githubWikiHomeworkTest(){
+        //open github selenide project
+        open("selenide/selenide");
+
+        //go to wiki page
+        $("#wiki-tab").click();
+
+        //open all pages
+        $("#wiki-pages-box").$("button.Link--muted").click();
+
+        //assert that SoftAssertions page is in Pages list
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+
+        //open SoftAssertions page
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+
+        //assert that page contains code example for JUnit 5
+        $("div.markdown-body").shouldHave(text("Using JUnit5 extend test class"));
+
+
+    }
+}
