b991775e5dcbd351d428841ee8f346663d73e56e
Kseniia Kuznetsova <ksandrasky@ya.ru>
2024-10-16 12:02:44 +0300

    assertion corrected

--- src/test/java/GithubWikiTest.java
+++ src/test/java/GithubWikiTest.java
@@ -34,7 +34,17 @@ public class GithubWikiTest {
         $("#wiki-pages-box").$(byText("SoftAssertions")).click();
 
         //assert that page contains code example for JUnit 5
-        $("div.markdown-body").shouldHave(text("Using JUnit5 extend test class"));
+        $("div.markdown-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
 
 
     }
