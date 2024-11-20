9e20c3db26b41c5a0e0bb928c3d4b0bf6b8dc9e6
Kseniia Kuznetsova <ksandrasky@ya.ru>
2024-11-20 21:03:34 +0300

    configure build

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -20,11 +20,14 @@ public class TestBase {
 
     @BeforeAll
     static void preconditionsForAllTests() {
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = false;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = System.getProperty("remoteUrl");
+        Configuration.browser = System.getProperty("browserName", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        //"https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
